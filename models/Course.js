3cfa9b5543efa1cfc2e7db01bac46c2649254bcc
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2022-12-26 00:05:13 +0200

    make the relation between Course and user

--- models/Course.js
+++ models/Course.js
@@ -1,25 +1,32 @@
 const { default: mongoose } = require("mongoose");
 
-const courseSchema= mongoose.Schema({
+const courseSchema = mongoose.Schema(
+  {
+    title: String,
+    price: Number,
+    instructor: { type: Schema.Types.ObjectId, ref: "User" },
+    descrption: String,
+    material: Array,
+    veidos: Array,
+    demo: String,
+    users: [{ 
+        type: mongoose.Schema.Types.ObjectId
+             , ref: "User" 
+            }],
 
-    title:String,
-    price:Number,
-    instructor:{type: Schema.Types.ObjectId, ref: 'User'},
-    descrption:String,
-    material:Array,
-    veidos:Array,
-    demo:String,
     category: {
-        type: String,
-        enum : ['theoretical','Practical','Summary'],
+      type: String,
+      enum: ["theoretical", "Practical", "Summary"],
     },
-    subCategory:{
-        type: String,
-        enum : ['first','second','Third','Fourth','general'],
+    subCategory: {
+      type: String,
+      enum: ["first", "second", "Third", "Fourth", "general"],
     },
-},
-{
-     versionKey:false,
-      strict:false,
-})
-module.exports=mongoose.model("Course",courseSchema);
+  },
+  {
+    versionKey: false,
+    strict: false,
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Course", courseSchema);
--- models/User.js
+++ models/User.js
@@ -1,64 +1,66 @@
 const mongoose = require("mongoose");
-const USerSchema = new mongoose.Schema({
-  Name: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-  },
-  userType:{
-    type:String,
-    enum:['admin' ,'subadmin', 'student' ,'instructor'],
-    default:"student"
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  phone: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  faculty: {
-    type: String,
-  },
-  Certificate: {
-    type: String,
-  },
-  band:{
-    type: String,
-  },
-  token:{
-  type:String
-  },
-  corsses: [
-    {
-      type: Object,
+const USerSchema = mongoose.Schema(
+  {
+    Name: {
+      type: String,
+      require: true,
     },
-  ],
-  commentes:[
-    {
-      type:String
-    }
-  ]
-});
-USerSchema.statics.isEmailuse=async function (email){
- if(!email) throw new Error('invalid Email');
- try {
-  const user =await this.findOne({email:email})
-  if(user) return false ;
-  return true ;
- } catch (error) {
-  console.log(`error inside isEmail method` ,error)
- }
-}
+    email: {
+      type: String,
+      require: true,
+    },
+    userType: {
+      type: String,
+      enum: ["admin", "subadmin", "student", "instructor"],
+      default: "student",
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    faculty: {
+      type: String,
+    },
+    Certificate: {
+      type: String,
+    },
+    band: {
+      type: String,
+    },
+    token: {
+      type: String,
+    },
+    corsses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    strict: false,
+  },
+  { timestamps: true }
+);
+USerSchema.statics.isEmailuse = async function (email) {
+  if (!email) throw new Error("invalid Email");
+  try {
+    const user = await this.findOne({ email: email });
+    if (user) return false;
+    return true;
+  } catch (error) {
+    console.log(`error inside isEmail method`, error);
+  }
+};
 
-
-module.exports=mongoose.model('User',USerSchema)
+module.exports = mongoose.model("User", USerSchema);

df7799cc295b08fd1d46735f251b9b5f727a0ed9
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2023-01-07 18:55:29 +0200

    add populte in user module

--- Controllers/UserCountroller.js
+++ Controllers/UserCountroller.js
@@ -74,7 +74,7 @@ module.exports = {
       const pagSize = 10;
       const pagNumber = Number(req.params.pagNumber * 1) || 1;
       const skip = (pagNumber - 1) * pagSize;
-      const data = await User.find({}).skip(skip).limit(pagSize);
+      const data = await User.find({}).skip(skip).limit(pagSize).populate({path:"Courses",select:['title', 'descrption']});
       const numOfPage = Math.ceil((await User.count()) / pagSize);
       if (data) {
         res
